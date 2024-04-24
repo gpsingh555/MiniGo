b726c5f221581b5012dc2c3f43f85bb3df4d4ecb
Gyanendra Pratap Singh <164019342+gpsingh555@users.noreply.github.com>
2024-04-24 18:19:09 +0530

    minigo_tags

--- minigo/urls.py
+++ minigo/urls.py
@@ -6,6 +6,10 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout, name='logout'),
     path('', views.dashboard_section_view, name='home'),
+    path('update-status/', views.update_status, name='update-status'),
+    path('get-custom-search/', views.get_custom_search, name='get-custom-search'),
+    path('campaign/', views.create_campaign_section_view, name='campaign'),
+
 
     
 ]
